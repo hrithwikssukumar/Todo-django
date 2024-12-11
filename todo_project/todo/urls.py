0cd293d5d7a933778e40d6e160cfa6fb27f39d85
Hrithwik <hrithwikssukumar@gmail.com>
2024-12-11 09:05:29 +0530

    completed the fuunctions

--- todo_project/todo/urls.py
+++ todo_project/todo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views  # Ensure views.py exists and contains relevant functions or classes.
+from . import views  
 
 urlpatterns = [
-    path('', views.list_todo_items, name='listitems'),  # Replace `index` with an actual view function.
+    path('', views.list_todo_items, name='listitems'),
+    path('update-items/<str:pk>/',views.update_todo_items,name= 'update-items') ,
+    path('delete-items/<str:pk>/',views.delete_todo_items,name= 'delete-items')
 ]
--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -1,14 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import *
+from .forms import *
 
 def list_todo_items(request):
-    return render(request,'todo/todolist.html')
 
-def add_todo_items(request):
-    pass
+    tasks = Task.objects.all()
 
-def delete_todo_items(request):
-    pass
+    form = TaskForm()
+
+    if request.method =='POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')    
+
+    context ={'tasks':tasks,'form':form}
+
+    return render(request,'todo/todolist.html',context)
+
+
+def update_todo_items(request,pk):
+
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == "POST":
+        task = TaskForm(request.POST,instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')    
+
+    context ={'task':task,'form':form}
+
+    return render(request,'todo/todoupdate.html',context)
+
+
+def delete_todo_items(request,pk):
+
+    item = Task.objects.get(id=pk)
+    if request.method =='POST':
+        item.delete()
+        return redirect('/')
+
+    context ={'item':item}
+
+    return render(request,'todo/tododelete.html',context)
 
 
 

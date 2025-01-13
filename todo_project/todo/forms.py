ba893ee84be8298f7ba98e5fff65368862ebd958
Hrithwik <hrithwikssukumar@gmail.com>
2025-01-13 21:25:24 +0530

    updated project

--- todo_project/todo/forms.py
+++ todo_project/todo/forms.py
@@ -1,7 +1,5 @@
 from django import forms
-
 from django.forms import ModelForm  
-
 from .models import Task  
      
 class TaskForm(forms.ModelForm):  
--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -4,47 +4,35 @@ from .models import *
 from .forms import *
 
 def list_todo_items(request):
-
     tasks = Task.objects.all()
-
     form = TaskForm()
-
     if request.method =='POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
         return redirect('/')    
-
     context ={'tasks':tasks,'form':form}
-
     return render(request,'todo/todolist.html',context)
 
 
 def update_todo_items(request,pk):
-
     task = Task.objects.get(id=pk)
     form = TaskForm(instance=task)
-
     if request.method == "POST":
         task = TaskForm(request.POST,instance=task)
         if form.is_valid():
             form.save()
         return redirect('/')    
-
     context ={'task':task,'form':form}
-
     return render(request,'todo/todoupdate.html',context)
 
 
 def delete_todo_items(request,pk):
-
     item = Task.objects.get(id=pk)
     if request.method =='POST':
         item.delete()
         return redirect('/')
-
     context ={'item':item}
-
     return render(request,'todo/tododelete.html',context)
 
 
